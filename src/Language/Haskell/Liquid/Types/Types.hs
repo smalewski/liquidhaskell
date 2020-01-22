ac1914fc462425ce521a197ddde29bc63f3791ed
Alfredo Di Napoli <alfredo@well-typed.com>
2020-01-22 12:48:51 +0100

    Make it build with 8.6.5 as well

--- src/Language/Haskell/Liquid/Types/Types.hs
+++ src/Language/Haskell/Liquid/Types/Types.hs
@@ -227,8 +227,7 @@ module Language.Haskell.Liquid.Types.Types (
   )
   where
 
--- import qualified ConLike                                as Ghc
--- import           InstEnv
+import           Language.Haskell.Liquid.GHC.API hiding (Expr, Target, isFunTy, LM)
 import           Class
 import           CoreSyn                                (CoreExpr)
 import           Data.String
@@ -236,14 +235,12 @@ import           DataCon
 import           GHC                                    (ModuleName, moduleNameString)
 import           GHC.Generics
 import           Module                                 (moduleNameFS)
--- import           NameSet
 import           PrelInfo                               (isNumericClass)
 import           Prelude                          hiding  (error)
 import qualified Prelude
 import           SrcLoc                                 (SrcSpan)
 import           TyCon
 import           Language.Haskell.Liquid.GHC.TypeRep
-import           Predicate                              (getClassPredTys_maybe)
 import           TysPrim                                (eqReprPrimTyCon, eqPrimTyCon)
 import           TysWiredIn                             (listTyCon, boolTyCon)
 import           Var
@@ -251,7 +248,6 @@ import           Var
 import           Control.Monad                          (liftM, liftM2, liftM3, liftM4)
 import           Control.DeepSeq
 import           Data.Bifunctor
---import           Data.Bifunctor.TH
 import           Data.Typeable                          (Typeable)
 import           Data.Generics                          (Data)
 import qualified Data.Binary                            as B
